20c6282f32965334635a115d02f21b8a26654cc7
udhayasudharsanan <udhayasudharsanank@gmail.com>
2024-09-28 19:41:32 +0530

    updated MongoDB connection string

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,23 @@
-const mongoose = require('mongoose');
-
-const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb+srv://SivaUdhi:UtSCnZrWPFjcOyiR@cluster0.psxhm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log('MongoDB Atlas connected');
-    } catch (err) {
-        console.error(err.message);
-        process.exit(1);
-    }
-};
-
-module.exports = connectDB;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://SivaUdhi:<db_password>@cluster0.psxhm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
